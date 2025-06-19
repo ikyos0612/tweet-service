201012fbc73d4afa945c8646480edc261d02a266
Kyosuke Isobe <105975728+ikyos0612@users.noreply.github.com>
2025-06-19 14:03:17 +0900

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TweetService from './app.js'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TweetService />
+  </React.StrictMode>
+)
